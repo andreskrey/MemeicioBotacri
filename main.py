481a74e5c3d555e26803b2c0a3fe612ccfefb980
Andres Rey <andres@providentcrm.com>
2017-01-28 12:05:08 +0000

    DB connection and saving

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+from peewee import *
 import praw
 import re
 import os
@@ -9,6 +12,8 @@ if os.environ.get('CURRENT_ENV') == 'HEROKU':
     client_secret = os.environ.get('CLIENT_SECRET')
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
+    mysql_username = os.environ.get['MYSQL_USERNAME']
+    mysql_password = os.environ.get['MYSQL_PASSWORD']
 else:
     credentials = open('credentials.json')
     credentials = json.load(credentials)
@@ -16,6 +21,25 @@ else:
     client_secret = credentials['CLIENT_SECRET']
     username = credentials['USERNAME']
     password = credentials['PASSWORD']
+    mysql_username = credentials['MYSQL_USERNAME']
+    mysql_password = credentials['MYSQL_PASSWORD']
+
+# Connect database
+db = MySQLDatabase('memes',
+                   user=mysql_username,
+                   passwd=mysql_password,
+                   host='localhost')
+
+
+class Memes(Model):
+    thing_id = CharField()
+    string = CharField()
+
+    class Meta:
+        database = db
+
+
+db.connect()
 
 # Connect to reddit
 reddit = praw.Reddit(user_agent='MemeicioBotacri',
@@ -30,5 +54,16 @@ comments = subreddit.comments()
 for comment in comments:
     string = comment.body
     # Loop over results
-    for meme in re.finditer('(\w+CIO\\b) (\w+CRI\\b)', string, re.IGNORECASE):
-        test=meme.group(0)
+    for memes in re.finditer('(\w+CIO\\b) (\w+CRI\\b)', string, re.IGNORECASE):
+        meme = memes.group(0)
+
+        # Filter Mauricio Macri (since it isn't dank enough)
+        if meme.lower() != 'mauricio macri':
+
+            # Check if we already are dank enough
+            query = Memes.select().where(Memes.string == meme)
+            if not query.exists():
+                meme = Memes(thing_id=comment.fullname, string=meme)
+                meme.save()
+
+test1 = 1
